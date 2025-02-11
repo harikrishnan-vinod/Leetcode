8081e9b4f8ebee0be8feddd1068d6689c56a6d01
Harikrishnan Vinod <harikrishnan1@hotmail.com>
2025-02-11 16:39:16 +0800

    SearchInsertPosition done

---
+++ SearchInsertPosition.java
@@ -0,0 +1,28 @@
+public class SearchInsertPosition
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        int r = nums.length;
+        int l = 0;
+        while(l < r)
+        {
+            final int pivot = (l + r)/2;
+            if(nums[pivot] == target)
+            {
+                return pivot;
+            }
+
+            if(nums[pivot] < target)
+            {
+                l = pivot + 1;
+            }
+
+            else
+            {
+                r = pivot;
+            }
+        }
+
+        return l;
+    }
+}
